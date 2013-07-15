8499c70fcdcf6cc399083676c7645314673cbbe8
jakezu <jakezu@gmail.com>
2013-07-16 01:09:50 +0300

    Core of the file

--- ImmersiveSpace.js
+++ ImmersiveSpace.js
@@ -4,8 +4,20 @@
 // !ref: masterclient.js
 // !ref: slaveclients.js
 
+var masterclient = "client1";
+
 // server side part
 if (server.IsRunning())
 {
-    engine.IncludeFile("server.js");
+	engine.IncludeFile("server.js");
 }
+
+// clients part
+else
+{
+	if (client.LoginProperty("username") == masterclient) 
+		engine.IncludeFile("masterclient.js");
+	else
+		engine.IncludeFile("slaveclients.js");
+}
+
