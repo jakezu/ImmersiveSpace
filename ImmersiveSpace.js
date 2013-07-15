580cebdbee13f79e2917afbd93245f47d74ddf33
jakezu <jakezu@gmail.com>
2013-07-16 01:35:16 +0300

    Added separated server and client files

--- ImmersiveSpace.js
+++ ImmersiveSpace.js
@@ -1,20 +1,26 @@
 /* ImmersiveSpace.js */
 
+/*
 // !ref: server.js
 // !ref: masterclient.js
 // !ref: slaveclients.js
+*/
+
+engine.IncludeFile("helpers.js");
 
 var masterclient = "client1";
 
 // server side part
 if (server.IsRunning())
 {
+	//console.LogInfo("server!");
 	engine.IncludeFile("server.js");
 }
 
 // clients part
 else
 {
+	//console.LogInfo("client!");
 	if (client.LoginProperty("username") == masterclient) 
 		engine.IncludeFile("masterclient.js");
 	else
---
+++ helpers.js
@@ -0,0 +1,26 @@
+// Define the basic log channel name if the using code wont be defining one.
+var _TUTORIAL_LOGCHANNEL = "Immersive_demo";
+ 
+// Helper function to tell us if we are running the script on a client instance.
+// In our case a client is a non-server and non-headless Tundra. Non-headless means we have the main UI window.
+function isClient()
+{
+    if (!server.IsRunning() && !framework.IsHeadless())
+        return true;
+    return false;
+}
+ 
+// Helper function to tell us if we are running the script on a server instance.
+function isServer()
+{
+    return server.IsRunning();
+}
+ 
+// Function to set log channel name.
+function SetLogChannel(name) { _TUTORIAL_LOGCHANNEL = name; }
+ 
+// Redirect functions to the ConsoleAPI logging functions. We also add the log channel name.
+function LogInfo(msg)        { console.LogInfo("[" + _TUTORIAL_LOGCHANNEL + "]: " + msg); }
+function LogWarning(msg)     { console.LogWarning("[" + _TUTORIAL_LOGCHANNEL + "]: " + msg); }
+function LogError(msg)       { console.LogError("[" + _TUTORIAL_LOGCHANNEL + "]: " + msg); }
+function Log(msg)            { LogInfo(msg); }
