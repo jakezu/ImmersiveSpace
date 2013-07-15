c5485a0a5ab0acd7b8f99d5d87395fceb5597fdc
jakezu <jakezu81@gmail.com>
2013-07-16 00:56:28 +0300

    First modification to ImmersiveSpace.js

--- ImmersiveSpace.js
+++ ImmersiveSpace.js
@@ -1,2 +1,11 @@
 /* ImmersiveSpace.js */
 
+// !ref: server.js
+// !ref: masterclient.js
+// !ref: slaveclients.js
+
+// server side part
+if (server.IsRunning())
+{
+    engine.IncludeFile("server.js");
+}
