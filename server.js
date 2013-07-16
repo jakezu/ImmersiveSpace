4001e5e5f754e40b33f1c3d73531f49199dbf18a
jakezu <jakezu@gmail.com>
2013-07-16 13:06:57 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 console.LogInfo("server!");
+
+var replicatedentity = scene.GetEntityByName("ReplicatedEntity");
+
+if (voidentity == null) {
+	voidentity = scene.CreateEntity(scene.NextFreeId(), 		/* NextFreeId() for replicated */
+					["EC_Placeable"],				/* Components */
+					'',							/* AttributeChange enum, 2 for LocalOnly, 3 for replicated. */
+					replicated=true, 					/* Replicate entity to server and other clients */
+					componentsReplicated=true);		/* Replicate components to server and other clients */
+	
+	voidentity.SetName("Void");
+	voidentity.SetTemporary(true);
+	console.LogInfo("Replicated server entity has been created with placeable component");
+}
+
+else
+	console.LogError("Server already running!");
