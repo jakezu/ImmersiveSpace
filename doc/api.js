13a03576cecd97d501273c7fcd96dfc874a80ad2
jakezu <jakezu81@gmail.com>
2015-08-06 23:13:23 +0300

    Docs

---
+++ doc/api.js
@@ -0,0 +1,13 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "LoaderClass",
+        "MasterClass",
+        "MovementModifier",
+        "ServerClass",
+        "SlaveClass"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
